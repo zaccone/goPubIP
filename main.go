be5d5e2f0e83f47c7164531bcc22d182f66b78dc
Marek Denis <marek.denis+zaccone@gmail.com>
2016-07-18 14:03:18 +0200

    Add new flags and log starting new server.

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"log"
+)
 
 var (
 	Port string
@@ -10,8 +13,14 @@ var (
 
 func init() {
 	flag.StringVar(&Port, "p", "5300", "Port to listen to")
+	flag.StringVar(&Port, "port", "5300", "Port to listen to")
+
 	flag.StringVar(&Host, "h", ".", "RR to response to")
+	flag.StringVar(&Host, "host", ".", "RR to response to")
+
 	flag.StringVar(&Addr, "a", "0.0.0.0", "Address to listen on")
+	flag.StringVar(&Addr, "address", "0.0.0.0", "Address to listen on")
+
 	flag.Parse()
 }
 
@@ -19,5 +28,6 @@ func main() {
 	resolver := &Resolver{
 		Port, Addr, Host,
 	}
+	log.Printf("Starting server at %v:%v, Host: %v\n", Addr, Port, Host)
 	resolver.Serve()
 }
