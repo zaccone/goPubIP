a97347a6fca63865bd03861e27d11064c2eccdfc
Marek Denis <marek.denis+zaccone@gmail.com>
2016-07-18 14:11:27 +0200

    Improve arg parameters help messages.

--- main.go
+++ main.go
@@ -12,14 +12,17 @@ var (
 )
 
 func init() {
-	flag.StringVar(&Port, "p", "5300", "Port to listen to")
-	flag.StringVar(&Port, "port", "5300", "Port to listen to")
+	PortStr := "Port to listen to"
+	flag.StringVar(&Port, "p", "5300", PortStr)
+	flag.StringVar(&Port, "port", "5300", PortStr)
 
-	flag.StringVar(&Host, "h", ".", "RR to response to")
-	flag.StringVar(&Host, "host", ".", "RR to response to")
+	HostStr := "RR to response to, host must end with a single dot ('.')"
+	flag.StringVar(&Host, "h", ".", HostStr)
+	flag.StringVar(&Host, "host", ".", HostStr)
 
-	flag.StringVar(&Addr, "a", "0.0.0.0", "Address to listen on")
-	flag.StringVar(&Addr, "address", "0.0.0.0", "Address to listen on")
+	AddrStr := "Address to listen on, mind that IPv6 address must be in format [ip6address]"
+	flag.StringVar(&Addr, "a", "0.0.0.0", AddrStr)
+	flag.StringVar(&Addr, "address", "0.0.0.0", AddrStr)
 
 	flag.Parse()
 }
@@ -28,6 +31,6 @@ func main() {
 	resolver := &Resolver{
 		Port, Addr, Host,
 	}
-	log.Printf("Starting server at %v:%v, Host: %v\n", Addr, Port, Host)
+	log.Printf("Starting server at %v:%v, Query host: %v\n", Addr, Port, Host)
 	resolver.Serve()
 }
